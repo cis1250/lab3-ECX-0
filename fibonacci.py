27b99804d1beaaf22fbc6bcb6dff4e89ef164bdc
Nico Ranin <155266232+ECX-0@users.noreply.github.com>
2025-09-30 10:57:58 -0400

    Finished
    Implement Fibonacci sequence calculation and user input.

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,25 @@
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+# Define Methods #
+
+def fibonacci(terms):
+ 	  # Debug # 
+		# print("Calculating: " + str(terms));
+    # Special Cases #
+    if (terms <= 1):
+        return 0;
+    elif (terms <= 2):
+        return 1;
+    # Calculation #
+    return fibonacci(terms - 2) + fibonacci(terms - 1)
+
+# Request Input #
+termAmount = 0;
+while (True):
+    inpt = input("Fibonacci term amount?")
+    if (inpt.isnumeric() and int(inpt) > 0):
+        termAmount = int(inpt);
+        break;
+		
+print(fibonacci(termAmount));
